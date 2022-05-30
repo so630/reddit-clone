7bca89e75a23726970259af3987d7671243cfdb1
so630 <73109255+so630@users.noreply.github.com>
2022-05-30 18:20:33 +0400

    i dont even know my code anymore but made changes for deployment

--- server/routes/authorization.js
+++ server/routes/authorization.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require("mysql");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
 const conn = mysql.createConnection({
